390e98bcff895d7b40b0ea0ddc5fe4e918f310d4
daria <gubskaya.dd@gmail.com>
2025-09-19 21:31:15 +0600

    try deploy

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
-const BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000";
+// const BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000";
+
+const BASE = import.meta.env.VITE_API_BASE || "https://3b88093531c0.ngrok-free.app";
 
 // Получить Telegram user ID (fallback на localStorage для dev)
 const getTelegramUserId = () => {
